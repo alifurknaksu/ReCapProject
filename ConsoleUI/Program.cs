8ffcb39d0ba8b0ea3e7619a2748b23d2409291b6
Ali Furkan Aksu <ali_furkan_aksu@hotmail.com>
2021-01-31 19:24:15 +0300

    Proje dosyası ekle.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description + "\nArabanın Günlük Kirası: " + car.DailyPrice);
+            }
+
+            Car car1 = carManager.GetbyId(1);
+            Console.WriteLine("\n\nId'si 1 olan Otomobilin Kirası: " + car1.DailyPrice);
+            Console.WriteLine("Id'si 1 olan Tesla Model X'in günlük kirası 600 olarak güncellendi. \n");
+            car1.DailyPrice = 600; 
+            carManager.Update(car1);
+
+            Car car6 = carManager.GetbyId(6);
+            Console.WriteLine("Id'si 6 olan Porsche Cayonne listeden silindi.\n");
+            carManager.Delete(car6);
+            
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description + "\nArabanın Günlük Kirası: " + car.DailyPrice);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemoryCarDal.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> {
+                new Car{Id=1,BrandId=1,ColorId=1,ModelYear=2020,DailyPrice=650,Description="Otomatik, Elektrikli, Otonom, 4 Koltuk, Tesla Model X"},
+                new Car{Id=2,BrandId=2,ColorId=2,ModelYear=2015,DailyPrice=250,Description="Manuel, Benzinli, Klimalı, 4 Koltuk, Navigasyonlu, Volvo S60 "},
+                new Car{Id=3,BrandId=3,ColorId=1,ModelYear=2018,DailyPrice=800,Description=" Yarı Otomatik, Benzinli, 2 Koltuk, Porsche Cayman"},
+                new Car{Id=4,BrandId=4,ColorId=2,ModelYear=2005,DailyPrice=100,Description="Yarı Otomatik, Benzin/LPG, 4 Koltuk, Mercedes Benz CLS 350 AMG"},
+                new Car{Id=5,BrandId=5,ColorId=1,ModelYear=1960,DailyPrice=1500,Description="Otomatik, Benzinli, 2 Koltuk , Chevrolet Corvette Stingray"},
+                new Car{Id=6,BrandId=3,ColorId=3,ModelYear=2021,DailyPrice=750,Description ="Yarı Otomatik, Dizel, 4 Koltuk, Porsche Cayenne"}
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToRemove = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToRemove);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int id)
+        {
+            return _cars.Single(c => c.Id == id);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+
+        }
+
+   
+    }
+}

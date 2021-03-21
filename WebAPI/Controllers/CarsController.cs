cefb559ccd614276c221533160f9f7e75183ebdf
Ali Furkan Aksu <ali_furkan_aksu@hotmail.com>
2021-03-21 23:02:23 +0300

    last

--- WebAPI/Controllers/CarsController.cs
+++ WebAPI/Controllers/CarsController.cs
@@ -44,6 +44,18 @@ namespace WebAPI.Controllers
             return BadRequest(result);
         }
 
+        [HttpGet("getbybrand")]
+        public IActionResult GetByBrand(int brandId)
+        {
+            var result = _carService.GetCarsByBrandId(brandId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
         [HttpPost("Add")]
         public IActionResult Add(Car car)
         {
